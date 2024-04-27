4baa2e077188b5d29340290131166337924fc356
jsx7ba <jscheiber@gmail.com>
2024-04-27 17:23:44 -0400

    add test for unsuccessful find

--- splay/splaytree_test.go
+++ splay/splaytree_test.go
@@ -1,9 +1,26 @@
 package splay
 
 import (
+	"cmp"
 	"fmt"
 	"testing"
 )
+
+func arrayEquals[E cmp.Ordered](t *testing.T, prefix string, expected, actual []E) bool {
+	if len(expected) != len(actual) {
+		t.Error(prefix, " actual array length does not match expected array")
+		return false
+	}
+
+	for i := range expected {
+		if expected[i] != actual[i] {
+			t.Error(prefix, "expected", expected[i], "got", actual[i], "at index", i)
+			return false
+		}
+	}
+
+	return true
+}
 
 func TestRotateLeft(t *testing.T) {
 	tree := NewSplayTree[int]()
@@ -78,7 +95,7 @@ func TestFind(t *testing.T) {
 	tree := NewSplayTree[int]()
 	tree.InsertAll(25, 50, 75, 30)
 
-	if !tree.Find(25) {
+	if !tree.Find(30) {
 		t.Error("could not find element 25")
 		return
 	}
@@ -86,11 +103,30 @@ func TestFind(t *testing.T) {
 	calls := 0
 	visitor := func(n *Node[int]) bool {
 		calls++
-		return n.value != 25
+		return n.value != 30
 	}
 	tree.Visit(visitor)
 
 	if calls != 1 {
 		t.Error("expected 1 call to find 25, got ", calls)
 	}
+}
+
+func TestNotFound(t *testing.T) {
+	expected := []int{75, 50, 30, 25}
+	tree := NewSplayTree[int]()
+	tree.InsertAll(30, 25, 75, 50)
+
+	if tree.Find(200) {
+		t.Error("200 shouldn't exist in the tree")
+	}
+
+	actual := make([]int, 0)
+	visitor := func(n *Node[int]) bool {
+		actual = append(actual, n.value)
+		return true
+	}
+	tree.Visit(visitor)
+
+	arrayEquals(t, "", expected, actual)
 }

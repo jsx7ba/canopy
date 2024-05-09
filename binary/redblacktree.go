3f6fdfb21ef710991b479da1d7976f4a0173152c
jsx7ba <jscheiber@gmail.com>
2024-05-09 18:33:23 -0400

    First cut at red black trees.  Still need to implement delete.

--- binary/redblacktree.go
+++ binary/redblacktree.go
@@ -1,46 +1,232 @@
 package binary
 
-import "cmp"
-
-type RBTree[E cmp.Ordered] struct {
-	root *RBNode[E]
-}
-
-type Color uint8
-
-const (
-	Black = iota
-	Red
+import (
+	"cmp"
+	"fmt"
 )
 
-type RBNode[E cmp.Ordered] struct {
-	value  E
-	parent *RBNode[E]
-	left   *RBNode[E]
-	right  *RBNode[E]
-	color  Color
+// RBTree a struct for red black trees.
+// Red Black Trees have the following properties:
+// 1) Every node is either red or black.
+// 2) nil nodes are considered black
+// 3) A red node cannot have a red child
+// 4) The path from any given node goes through the same number of black nodes.
+// 5) If a node has a single child, it must be a red child.
+type RBTree[E cmp.Ordered] struct {
+	root *rbNode[E]
 }
 
-func (n *RBNode[E]) Value() E {
+type color uint8
+
+const (
+	black = iota
+	red
+)
+
+func (c color) String() string {
+	if c == black {
+		return "black"
+	}
+	return "red"
+}
+
+type rbNode[E cmp.Ordered] struct {
+	value  E
+	parent *rbNode[E]
+	left   *rbNode[E]
+	right  *rbNode[E]
+	color  color
+}
+
+func (n *rbNode[E]) Value() E {
 	return n.value
 }
 
-func (n *RBNode[E]) Parent() *RBNode[E] {
-	return n.parent
+func (n *rbNode[E]) p() (Node[E], bool) {
+	return n.parent, n.parent != nil
 }
 
-func (n *RBNode[E]) Left() *RBNode[E] {
-	return n.left
+func (n *rbNode[E]) l() (Node[E], bool) {
+	return n.left, n.left != nil
 }
 
-func (n *RBNode[E]) Right() *RBNode[E] {
-	return n.right
+func (n *rbNode[E]) r() (Node[E], bool) {
+	return n.right, n.right != nil
 }
 
 type RedBlackTree[E cmp.Ordered] struct {
-	root *RBNode[E]
+	root *rbNode[E]
 }
 
+// NewRedBlackTree creates a new red black tree.
 func NewRedBlackTree[E cmp.Ordered]() *RedBlackTree[E] {
 	return &RedBlackTree[E]{root: nil}
+}
+
+func (t *RedBlackTree[E]) Insert(value E) bool {
+	node := &rbNode[E]{value: value, color: red}
+	if t.root == nil {
+		t.root = node
+		node.color = black
+		return true
+	}
+
+	current := t.root
+	for {
+		if value < current.value {
+			if current.left == nil {
+				current.left = node
+				node.parent = current
+				break
+			}
+			current = current.left
+		} else if value > current.value {
+			if current.right == nil {
+				current.right = node
+				node.parent = current
+				break
+			}
+			current = current.right
+		} else {
+			return false
+		}
+	}
+
+	t.balance(node)
+	return true
+}
+
+func (t *RedBlackTree[E]) balance(n *rbNode[E]) {
+	if n == t.root || n.parent == nil {
+		return
+	}
+
+	for n != t.root && n.parent.color == red {
+		p := n.parent
+		gp := n.parent.parent
+
+		if gp == nil {
+			// ?? not sure
+			break
+		}
+
+		u := gp.right
+		if p == gp.right {
+			u = gp.left
+		}
+
+		if u != nil && u.color == red { // Case 1: The parent color is red, and the uncle color is red
+			recolor1(p, u, gp)
+			fmt.Println("case 1 recolor")
+		} else { // Case 2: the parent color is red and the uncle color is black (or nil)
+			if n == p.right && p == gp.left { // Case 2: n, p and gp make a triangle - rotate around parent
+				t.rotateLeft(p)
+				fmt.Println("case 2 rotate left")
+			} else if n == p.left && p == gp.right {
+				t.rotateRight(p)
+				fmt.Println("case 2 rotate right")
+			} else if n == p.right && p == gp.right { // Case 3: n, p, and gp are in a line: rotate around grandparent
+				t.rotateLeft(gp)
+				recolor3(p, gp)
+				fmt.Println("case 3 rotate left/recolor")
+			} else if n == p.left && p == gp.left {
+				t.rotateRight(gp)
+				recolor3(p, gp)
+				fmt.Println("case 3 rotate right/recolor")
+			}
+		}
+		n = p
+	}
+	t.root.color = black
+}
+
+func recolor1[E cmp.Ordered](p, u, gp *rbNode[E]) {
+	p.color = black
+	u.color = black
+	gp.color = red
+}
+
+func recolor3[E cmp.Ordered](p, gp *rbNode[E]) {
+	p.color = black
+	gp.color = red
+}
+
+func (t *RedBlackTree[E]) rotateLeft(n *rbNode[E]) {
+	p := n.parent
+	c := n.right
+	c.parent = n.parent
+	n.parent = c
+	n.right = c.left
+	c.left = n
+	if n.right != nil {
+		n.right.parent = n
+	}
+
+	if p != nil {
+		if p.left == n {
+			p.left = c
+		} else {
+			p.right = c
+		}
+	}
+
+	if c.parent == nil {
+		t.root = c
+	}
+}
+
+func (t *RedBlackTree[E]) rotateRight(n *rbNode[E]) {
+	p := n.parent
+
+	c := n.left
+	c.parent = n.parent
+	n.parent = c
+	n.left = c.right
+	c.right = n
+	if n.left != nil {
+		n.left.parent = n
+	}
+
+	if p != nil {
+		if p.left == n {
+			p.left = c
+		} else {
+			p.right = c
+		}
+	}
+
+	if c.parent == nil {
+		t.root = c
+	}
+}
+
+func rbfind[E cmp.Ordered](n *rbNode[E], value E) *rbNode[E] {
+	for n != nil && n.value != value {
+		if value < n.value {
+			n = n.left
+		} else if value > n.value {
+			n = n.right
+		}
+	}
+	return n
+}
+
+func (t *RedBlackTree[E]) Delete(value E) bool {
+	node := rbfind(t.root, value)
+	if node == nil {
+		return false
+	}
+	// todo - rebalance
+	return true
+}
+
+func (t *RedBlackTree[E]) Find(value E) bool {
+	node := rbfind(t.root, value)
+	return node != nil
+}
+
+func (t *RedBlackTree[E]) Traverse(method func(node Node[E], v func(node Node[E]) bool) bool, v func(node Node[E]) bool) {
+	if t.root != nil {
+		method(t.root, v)
+	}
 }
---
+++ binary/redblacktree_test.go
@@ -0,0 +1,91 @@
+package binary
+
+import (
+	"cmp"
+	"fmt"
+	"testing"
+)
+
+func TestRedBlack_singleNode(t *testing.T) {
+	tree := NewRedBlackTree[int]()
+	if !tree.Insert(32) {
+		t.Fatal("insert single node failed")
+	}
+
+	count := 0
+	counter := func(node Node[int]) bool {
+		count++
+		return true
+	}
+
+	tree.Traverse(PreOrder[int], counter)
+	if count != 1 {
+		PrintTree[int](tree)
+		t.Fatal("counted more than one node, or zero nodes")
+	}
+}
+
+func TestRedBlack_Case3Right(t *testing.T) {
+	tree := NewRedBlackTree[int]()
+	tree.Insert(32)
+	tree.Insert(42)
+	tree.Insert(52)
+
+	n := tree.root
+	if fail, mesg := checkNode[int](n, 42, black); fail {
+		t.Fatal(mesg)
+	}
+	if fail, mesg := checkNode[int](n.left, 32, red); fail {
+		t.Fatal(mesg)
+	}
+	if fail, mesg := checkNode[int](n.right, 52, red); fail {
+		t.Fatal(mesg)
+	}
+}
+
+func TestRedBlack_Case2Left(t *testing.T) {
+
+}
+
+func checkNode[E cmp.Ordered](n *rbNode[E], value E, color color) (bool, string) {
+	if n == nil {
+		return true, "node is nil"
+	}
+
+	if n.value != value {
+		return true, fmt.Sprintf("node has value %v expected %v", n.value, value)
+	}
+
+	if n.color != color {
+		return true, fmt.Sprintf("node has color %s, expected %s", n.color, color)
+	}
+
+	return false, ""
+}
+
+func TestRedBlack_fourNodes(t *testing.T) {
+	tree := NewRedBlackTree[int]()
+	InsertAll(tree, 32, 42, 52, 49, 53, 54, 15, 17)
+	printRBTree[int](tree)
+}
+
+// provide visual confirmation about node color
+func printRBTree[E cmp.Ordered](tree *RedBlackTree[E]) {
+	printer := func(n Node[E]) bool {
+		node := n.(*rbNode[E])
+		template := "%v(%s) L %v R %v\n"
+		leftValue := "nil"
+		rightValue := "nil"
+		if node.left != nil {
+			leftValue = fmt.Sprintf("%+v", node.left.value)
+		}
+		if node.right != nil {
+			rightValue = fmt.Sprintf("%v", node.right.value)
+		}
+
+		fmt.Printf(template, node.value, node.color, leftValue, rightValue)
+		return true
+	}
+
+	tree.Traverse(BreadthFirst[E], printer)
+}
